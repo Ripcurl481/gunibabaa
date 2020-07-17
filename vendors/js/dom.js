5f63151329020290253636d1b7e4288b9f8f2660
Sandesh Neupane <halfpacearial481@gmail.com>
2020-07-16 20:57:46 -0700

    record

---
+++ vendors/js/dom.js
@@ -0,0 +1,67 @@
+
+// document.getElementById("header").addEventListener("click" , function abc (){
+//     this.style.border = "10px solid white";
+// })
+// document.getElementById("header").classList.add("xyz" , "byz") = function abc() {
+//     var a = document.getElementById("header").classList;
+//     console.log(a);
+    
+    
+// }
+
+// abc();
+// document.getElementByID().addEventListener("click",abc);
+
+//  var a = document.getElementById("header").children.style.background = 'green';
+
+//  var a = document.body.parentElement;
+//  console.log(a);
+// var newElement = document.createElement("h2");
+// var newText = document.createTextNode("We love the element");
+
+
+
+// newElement.appendChild(newText);
+// document.getElementById("header").appendChild(newElement);
+// var target = target.insertBefore(newElement,target.childNodes);
+// console.log(target);
+
+
+// console.log(newElement);
+// var john = {
+//     name: "John",
+//     yearofBirth: 1989,
+//     job: 'teacher'
+// };
+// var Person = function(name,yearofBirth,job){
+//     this.name = name;
+//     this.yearofBirth = yearofBirth;
+//     this.job = job;
+// };
+// var john = new Person('john' , '1990' , 'teacher');
+// console.log(john);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
